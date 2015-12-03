aa5ad68696118355f1de517e2b927bb99bb96b0f
Mary Kergosien <mary@kergosien.net>
2015-12-02 20:36:45 -0600

    Fixed lib paths

--- build.js
+++ build.js
@@ -1,15 +1,15 @@
 ({
   baseUrl: "./app",
   paths: {
-    angularRoute: "lib/angular-route/angular-route",
-    angularfire: "lib/angularfire/dist/angularfire.min",
-    angularFilter: "lib/angular-filter/dist/angular-filter.min",
-    jquery: "lib/jquery/dist/jquery.min",
-    angular: "lib/angular/angular.min",
-    firebase: "lib/firebase/firebase",
-    bootstrap: "lib/bootstrap/dist/js/bootstrap.min",
-    babel: "lib/requirejs-babel/babel-5.8.22.min",
-    es6:"lib/requirejs-babel/es6"
+    angularRoute: "/lib/angular-route/angular-route",
+    angularfire: "/lib/angularfire/dist/angularfire.min",
+    angularFilter: "/lib/angular-filter/dist/angular-filter.min",
+    jquery: "/lib/jquery/dist/jquery.min",
+    angular: "/lib/angular/angular.min",
+    firebase: "/lib/firebase/firebase",
+    bootstrap: "/lib/bootstrap/dist/js/bootstrap.min",
+    babel: "/lib/requirejs-babel/babel-5.8.22.min",
+    es6:"/lib/requirejs-babel/es6"
   },
   shim: {
     'angularfire': ["angular", "firebase"],
