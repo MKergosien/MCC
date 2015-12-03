3b8f069aa0122a70d8cc303242f4db1535ae1ac2
Mary Kergosien <mary@kergosien.net>
2015-12-02 20:16:24 -0600

    Removed bower path

--- app/require-config.js
+++ app/require-config.js
@@ -1,14 +1,14 @@
 require.config({
   paths: {
-    angularRoute: "../lib/bower_components/angular-route/angular-route",
-    angularfire: "../lib/bower_components/angularfire/dist/angularfire.min",
-    angularFilter: "../lib/bower_components/angular-filter/dist/angular-filter.min",
-    jquery: "../lib/bower_components/jquery/dist/jquery.min",
-    angular: "../lib/bower_components/angular/angular.min",
-    firebase: "../lib/bower_components/firebase/firebase",
-    bootstrap: "../lib/bower_components/bootstrap/dist/js/bootstrap.min",
-    babel: "../lib/bower_components/requirejs-babel/babel-5.8.22.min",
-    es6:"../lib/bower_components/requirejs-babel/es6"
+    angularRoute: "lib/angular-route/angular-route",
+    angularfire: "lib/angularfire/dist/angularfire.min",
+    angularFilter: "lib/angular-filter/dist/angular-filter.min",
+    jquery: "lib/jquery/dist/jquery.min",
+    angular: "lib/angular/angular.min",
+    firebase: "lib/firebase/firebase",
+    bootstrap: "lib/bootstrap/dist/js/bootstrap.min",
+    babel: "lib/requirejs-babel/babel-5.8.22.min",
+    es6:"lib/requirejs-babel/es6"
   },
   shim: {
     'angularfire': ["angular", "firebase"],
